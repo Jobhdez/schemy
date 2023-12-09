c67fa4ab2dd30fa28de223e1e9db5c54680a3dcb
Job Hernandez Lara <hj93@protonmail.com>
2023-12-08 23:32:17 -0800

    move dir

--- src/repl.py
+++
@@ -1,8 +0,0 @@
-from parser import parser
-from interp import interp
-
-def repl(prompt='lambda> '):
-    while True:
-        tree = parser.parse(input(prompt))
-        val = interp(tree)
-        print(val)
