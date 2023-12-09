6c0ee41b24af3da58c15bfbe78b1e0bb0e41355d
Job Hernandez Lara <hj93@protonmail.com>
2023-12-08 17:27:29 -0800

    make env global

--- repl.py
+++ repl.py
@@ -1,8 +1,9 @@
 from parser import parser
 from interp import interp
 
+env = {}
 def repl(prompt='lambda> '):
     while True:
         tree = parser.parse(input(prompt))
-        val = interp(tree, {})
+        val = interp(tree, env)
         print(val)
