742be30ad3c2204fa1602288b359f496832421d8
Job Hernandez Lara <hj93@protonmail.com>
2023-12-09 17:07:02 -0800

    rename dir

---
+++ scheme_interp/repl.py
@@ -0,0 +1,8 @@
+from parser import parser
+from interp import interp
+
+def repl(prompt='lambda> '):
+    while True:
+        tree = parser.parse(input(prompt))
+        val = interp(tree)
+        print(val)
